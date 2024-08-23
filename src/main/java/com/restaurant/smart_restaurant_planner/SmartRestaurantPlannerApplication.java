4e17689e99e729d5002d2726b3dcc228d1355fbe
la paix <dj.lapaix.tech@gmail.com>
2024-08-23 15:05:17 +0200

    pushing my project

---
+++ src/main/java/com/restaurant/smart_restaurant_planner/SmartRestaurantPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.smart_restaurant_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRestaurantPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRestaurantPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant/smart_restaurant_planner/SmartRestaurantPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.smart_restaurant_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRestaurantPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
